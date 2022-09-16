a8f853f130f321962ba39f7c32ded2d6ad5e3f5d
Sergey Intyashev <seintyashev@mail.ru>
2022-09-16 16:49:31 +0400

    Add Tag model

--- chef/blog/models.py
+++ chef/blog/models.py
@@ -15,3 +15,8 @@ class Category(MPTTModel):
 
     class MPTTMeta:
         order_insertion_by = ['name']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
