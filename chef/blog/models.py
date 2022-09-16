184730bd627bea6e38197db2335486bfa93851f5
Sergey Intyashev <seintyashev@mail.ru>
2022-09-16 16:49:07 +0400

    Add Category model

---
+++ chef/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    parent = TreeForeignKey(
+        'self',
+        related_name='children',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
