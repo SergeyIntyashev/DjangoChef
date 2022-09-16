c1a53797bf9f20dc0b53a3a1af00903757560851
Sergey Intyashev <seintyashev@mail.ru>
2022-09-16 16:53:39 +0400

    Add Post model

--- chef/blog/models.py
+++ chef/blog/models.py
@@ -20,3 +20,17 @@ class Category(MPTTModel):
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='articles/')
+    text = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        related_name='post',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    tags = models.ManyToManyField(Tag, related_name='post')
+    created_at = models.DateTimeField(auto_now_add=True)
