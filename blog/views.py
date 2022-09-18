d129796409c39c2e97055ce8b67ef8b4fec833f5
Sergey Intyashev <seintyashev@mail.ru>
2022-09-18 22:21:49 +0400

    Add PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+
+from blog.models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects. \
+            select_related('category'). \
+            filter(category__slug=self.kwargs.get('slug'))
 
 
 def home(request):
