d04b92d52f82fb666ef670fe88f7d9cd4d718963
Sergey Intyashev <seintyashev@mail.ru>
2022-09-18 23:20:46 +0400

    Add url for post detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 
 urlpatterns = [
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
+    path('<slug:slug>/<slug:post_slug>', views.PostDetailView.as_view(), name='post_detail'),
     path('', views.home)
 ]
